a692f89ee258b5eb1865f2d78b533dd3353333a4
Miroslav Smukov <msmukov@brandstate.com.au>
2016-02-11 20:02:41 +0100

    Prepared a reference web-page template, and started adding entities

--- BasketStats/src/basket/basketStats.py
+++ BasketStats/src/basket/basketStats.py
@@ -18,23 +18,23 @@ if __name__ == '__main__':
     metamodel_export(basket_metamodel, 'basketStats_meta.dot')
     graph = pydot.graph_from_dot_file('basketStats_meta.dot')
     graph.write_png('basketStats_meta.png')
-    
+
     #===========================================================================
     # object_processors = {'MoveCommand' : move_command_processor}
     # forma_mm.register_obj_processors(object_processors)
     #===========================================================================
-    
+
     basket_model = basket_metamodel.model_from_file('game.tx')
     model_export(basket_model, 'basketStats_model.dot')
     graph = pydot.graph_from_dot_file('basketStats_model.dot')
     graph.write_png('basketStats_model.png')
-    
+
     #===========================================================================
     # forma = Forma()
     # forma.interpret(forma_model)
     # print(forma)
     #===========================================================================
-    
+
     #===========================================================================
     # env = Environment(trim_blocks = True, lstrip_blocks = True, loader = PackageLoader("forma","templates"))
     # env.filters['componentType'] = componentType
---
+++ BasketStats/src/webPageTester.py
@@ -0,0 +1,17 @@
+
+
+import os, sys
+from entities import entities
+
+print(sys.version)
+
+ref1 = entities.Person("MIGUEL", "PEREZ", "ESP")
+print(ref1.first_name)
+print(ref1.full_name())
+
+# Jinja2 Test
+from jinja2 import Template
+t = Template("Hello {{ something }}!")
+print(t.render(something="World"))
+t = Template("My favorite numbers: {% for n in range(1,10) %}{{n}} " "{% endfor %}")
+print(t.render())
