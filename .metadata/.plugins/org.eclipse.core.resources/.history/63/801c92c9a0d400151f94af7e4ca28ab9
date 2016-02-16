a1c1c798ff524443a67908432f7a92cf245c2f26
d.popovic89@gmail.com <d.popovic@gmail.com>
2016-02-16 13:11:27 +0100

    Editor za JSD inicijalizacija

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/63/801c92c9a0d400151f94af7e4ca28ab9
@@ -0,0 +1,14 @@
+package application;
+
+import view.MainFrame;
+
+public class Application {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MainFrame mf=new MainFrame();
+		mf.setVisible(true);
+
+	}
+
+}
--- BasketApplication/src/application/Application.java
+++ BasketApplication/src/application/Application.java
@@ -1,14 +1,16 @@
 package application;
 
+import editor.view.EditorFrame;
 import view.MainFrame;
 
 public class Application {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		MainFrame mf=new MainFrame();
-		mf.setVisible(true);
-
+		/*MainFrame mf=new MainFrame();
+		mf.setVisible(true);*/
+		EditorFrame mg = new EditorFrame();
+		mg.setVisible(true);
 	}
 
 }
