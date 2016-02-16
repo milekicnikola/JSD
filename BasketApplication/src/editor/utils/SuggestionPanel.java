ec463ff32636cd471e95ffacc1536b2d956bb53c
d.popovic89@gmail.com <d.popovic@gmail.com>
2016-02-16 22:05:48 +0100

    Autocomplete

--- BasketApplication/src/editor/utils/SuggestionPanel.java
+++ BasketApplication/src/editor/utils/SuggestionPanel.java
@@ -5,28 +5,39 @@ import java.awt.Color;
 import java.awt.Point;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
 import javax.swing.JList;
 import javax.swing.JPopupMenu;
-import javax.swing.JTextArea;
+import javax.swing.JTextPane;
 import javax.swing.ListSelectionModel;
 import javax.swing.SwingUtilities;
 import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultHighlighter;
+import javax.swing.text.DocumentFilter;
+import javax.swing.text.Highlighter;
+import javax.swing.text.Style;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyleContext;
 
 
 
 public class SuggestionPanel {
-    
-	private JList list;
+    private JList list;
     private JPopupMenu popupMenu;
     private String subWord;
-    private final int insertionPosition;
+    private int insertionPosition;
+    public SuggestionPanel suggestion;
+	public JTextPane txtpnEditor;
+	public int newLines;
 
-    private SuggestionPanel suggestion;
-    private JTextArea textarea;
-    
-    public SuggestionPanel(JTextArea textarea, int position, String subWord, Point location) {
+    public SuggestionPanel(JTextPane txtpnEditor, int position, String subWord, Point location, int newLines) {
         this.insertionPosition = position;
         this.subWord = subWord;
         popupMenu = new JPopupMenu();
@@ -34,7 +45,9 @@ public class SuggestionPanel {
         popupMenu.setOpaque(false);
         popupMenu.setBorder(null);
         popupMenu.add(list = createSuggestionList(position, subWord), BorderLayout.CENTER);
-        popupMenu.show(textarea, location.x, textarea.getBaseline(0, 0) + location.y);
+        popupMenu.show(txtpnEditor, location.x, txtpnEditor.getBaseline(0, 0) + location.y+20);
+        this.txtpnEditor=txtpnEditor;
+        this.newLines=newLines;
     }
 
     public void hide() {
@@ -43,19 +56,63 @@ public class SuggestionPanel {
             suggestion = null;
         }
     }
+    
+    
+    
 
     private JList createSuggestionList(final int position, final String subWord) {
+    	
+    	ArrayList<String> reservedWords = new ArrayList<String>();
+    	try {
+			FileInputStream fstream = new FileInputStream("resources/reserved_words.txt");
+			BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+
+			String strLine;
+
+			//Read File Line By Line
+			while ((strLine = br.readLine()) != null)   {
+			  // Print the content on the console
+			  //System.out.println (strLine);
+			  if(!strLine.isEmpty() && strLine!="")
+				  reservedWords.add(strLine);
+			}
+
+			//Close the input stream
+			br.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	//reservedWords.get(0).contains(s)
+    	
         Object[] data = new Object[10];
-        for (int i = 0; i < data.length; i++) {
-            data[i] = subWord + i+"test";
+        int counter=0;
+        for(String s: reservedWords){
+        	String reserved = s;
+        	if(reserved.toLowerCase().contains(subWord.toLowerCase())){
+        		data[counter]=s;
+        		System.out.println(s);
+        		counter++;
+        	}
         }
+        
+        
+        /*for (int i = 0; i < data.length; i++) {
+            data[i] = subWord + i+"test";
+        }*/
         JList list = new JList(data);
-        list.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY, 1));
+        list.setBorder(BorderFactory.createLineBorder(Color.RED, 1));
         list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         list.setSelectedIndex(0);
         list.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
+            	//autocomplete after step
+            	System.out.println(e);
                 if (e.getClickCount() == 2) {
                     insertSelection();
                 }
@@ -63,12 +120,26 @@ public class SuggestionPanel {
         });
         return list;
     }
-
+    private Highlighter.HighlightPainter cyanPainter;
+    private Highlighter.HighlightPainter redPainter;
     public boolean insertSelection() {
         if (list.getSelectedValue() != null) {
             try {
-                final String selectedSuggestion = ((String) list.getSelectedValue()).substring(subWord.length());
-                textarea.getDocument().insertString(insertionPosition, selectedSuggestion, null);
+                final String selectedSuggestion = ((String) list.getSelectedValue());//.substring(subWord.length());
+               /* cyanPainter = new DefaultHighlighter.DefaultHighlightPainter(Color.cyan);
+                redPainter = new DefaultHighlighter.DefaultHighlightPainter(Color.red);*/
+                System.out.println((String) list.getSelectedValue());
+                /*txtpnEditor.getHighlighter().addHighlight(0, 3, DefaultHighlighter.DefaultPainter);
+                txtpnEditor.getHighlighter().addHighlight(8, 14, cyanPainter);*/
+                //txtpnEditor.getText().substring(insertionPosition-subWord.length(), insertionPosition);
+                insertionPosition = insertionPosition-newLines;
+                System.out.println(insertionPosition);
+                int offset = insertionPosition-subWord.length();
+                int pos = subWord.length();
+                
+                txtpnEditor.getDocument().remove(offset, pos);
+                txtpnEditor.getDocument().insertString(insertionPosition-subWord.length(), selectedSuggestion, null);
+                
                 return true;
             } catch (BadLocationException e1) {
                 e1.printStackTrace();
@@ -78,6 +149,11 @@ public class SuggestionPanel {
         return false;
     }
 
+    private void hideSuggestion() {
+        if (suggestion != null) {
+            suggestion.hide();
+        }
+    }
     public void moveUp() {
         int index = Math.min(list.getSelectedIndex() - 1, 0);
         selectIndex(index);
@@ -89,19 +165,13 @@ public class SuggestionPanel {
     }
 
     private void selectIndex(int index) {
-        final int position = textarea.getCaretPosition();
+        final int position = txtpnEditor.getCaretPosition();
         list.setSelectedIndex(index);
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                textarea.setCaretPosition(position);
+            	txtpnEditor.setCaretPosition(position);
             };
         });
-    }
-    
-    private void hideSuggestion() {
-        if (suggestion != null) {
-            suggestion.hide();
-        }
     }
 }
